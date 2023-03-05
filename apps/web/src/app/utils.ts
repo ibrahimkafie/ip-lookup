d22426c7d6fbd85bdee7a28a2bfbf02e75af3621
Ibrahim Kafie <imq.kafie@gmail.com>
2023-03-05 18:13:48 -0500

    clean utils return type

--- apps/web/src/app/utils.ts
+++ apps/web/src/app/utils.ts
@@ -1,12 +1,17 @@
 import { IPInfo } from "@max-mind/core/models";
 
+export interface IPCacheData {
+    cachedIPs: IPInfo[];
+    nonCachedIPs: string[];
+}
+
 /**
  * Retrieves cached IP(s) information from sessionStorage and separates it from the non-cached IPs.
  * 
  * @param ipAddresses An array of IP addresses to look up in sessionStorage.
  * @returns An object containing an array of cached IP information and an array of non-cached IP addresses.
  */
-export function getCachedIPsData(ipAddresses: string[]) {
+export function getCachedIPsData(ipAddresses: string[]): IPCacheData {
     const cachedIPs: IPInfo[] = [];
     const nonCachedIPs: string[] = [];
 
@@ -28,7 +33,7 @@ export function getCachedIPsData(ipAddresses: string[]) {
  *
  * @param ipItems An array of IPInfo objects to cache in sessionStorage.
  */
-export function setCachedIPsData(ipItems: IPInfo[]) {
+export function setCachedIPsData(ipItems: IPInfo[]): void {
     ipItems.forEach((item: IPInfo) => {
         sessionStorage.setItem(item.ip, JSON.stringify(item));
     });
@@ -41,7 +46,7 @@ export function setCachedIPsData(ipItems: IPInfo[]) {
  * @param sortByArray An array of IP addresses to use for sorting the targetArray.
  * @returns A sorted array of IPInfo objects.
  */
-export function sortBasedOnArray(targetArray: IPInfo[], sortByArray: string[]) {
+export function sortBasedOnArray(targetArray: IPInfo[], sortByArray: string[]): IPInfo[] {
     return targetArray.sort((a, b) => {
         return sortByArray.indexOf(a.ip) - sortByArray.indexOf(b.ip);
     })
